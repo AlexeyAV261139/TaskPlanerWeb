a8c4aa3daee76ad8a9879c3d5168de483110dcab
Alexey <Alexey261139@mail.ru>
2024-01-25 18:48:00 +0300

    Изменено. Добавлена бд

---
+++ TaskPlaner/MyTask.cs
@@ -0,0 +1,6 @@
+﻿public class MyTask
+{
+    public string Id { get; set; } = "";
+    public string Content { get; set; } = "";
+}
+
--- TaskPlaner/Program.cs
+++ TaskPlaner/Program.cs
@@ -1,28 +1,20 @@
 using System.Text.RegularExpressions;
- 
-// начальные данные
-List<MyTask> myTasks = new List<MyTask> 
-{ 
-    new() { Id = Guid.NewGuid().ToString(), Content = "”борка"},
-    new() { Id = Guid.NewGuid().ToString(), Content = "–абота" },
-    new() { Id = Guid.NewGuid().ToString(), Content = "”чЄба" }
-};
- 
+
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
- 
+
 app.Run(async (context) =>
 {
     var response = context.Response;
     var request = context.Request;
     var path = request.Path;
     //string expressionForNumber = "^/api/users/([0-9]+)$";   // если id представл€ет число
- 
+
     // 2e752824-1657-4c7f-844b-6ec2e168e99c
     string expressionForGuid = @"^/api/tasks/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
-    if (path == "/api/tasks" && request.Method=="GET")
+    if (path == "/api/tasks" && request.Method == "GET")
     {
-        await GetAllMyTasks(response); 
+        await GetAllMyTasks(response);
     }
     else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "GET")
     {
@@ -49,48 +41,59 @@ app.Run(async (context) =>
         await response.SendFileAsync("html/index.html");
     }
 });
- 
+
 app.Run();
- 
+
 // получение всех пользователей
 async Task GetAllMyTasks(HttpResponse response)
 {
-    await response.WriteAsJsonAsync(myTasks);
+    using ApplicationContext db = new ApplicationContext();
+    {
+        await response.WriteAsJsonAsync(db.MyTasks);
+    }
 }
 // получение одного пользовател€ по id
 async Task GetMyTask(string? id, HttpResponse response)
 {
-    // получаем пользовател€ по id
-    MyTask? myTask = myTasks.FirstOrDefault((t) => t.Id == id);
-    // если пользователь найден, отправл€ем его
-    if (myTask != null)
-        await response.WriteAsJsonAsync(myTask);
-    // если не найден, отправл€ем статусный код и сообщение об ошибке
-    else
+    using (ApplicationContext db = new ApplicationContext())
     {
-        response.StatusCode = 404;
-        await response.WriteAsJsonAsync(new { message = "«адача не найдена" });
+        // получаем пользовател€ по id
+        MyTask? myTask = db.MyTasks.FirstOrDefault((t) => t.Id == id);
+        // если пользователь найден, отправл€ем его
+        if (myTask != null)
+            await response.WriteAsJsonAsync(myTask);
+        // если не найден, отправл€ем статусный код и сообщение об ошибке
+        else
+        {
+            response.StatusCode = 404;
+            await response.WriteAsJsonAsync(new { message = "«адача не найдена" });
+        }
     }
 }
- 
+
 async Task DeleteMyTask(string? id, HttpResponse response)
 {
-    // получаем пользовател€ по id
-    MyTask? MyTask = myTasks.FirstOrDefault((t) => t.Id == id);
-    // если пользователь найден, удал€ем его
-    if (MyTask != null)
+    using (ApplicationContext db = new ApplicationContext())
     {
-        myTasks.Remove(MyTask);
-        await response.WriteAsJsonAsync(MyTask);
-    }
-    // если не найден, отправл€ем статусный код и сообщение об ошибке
-    else
-    {
-        response.StatusCode = 404;
-        await response.WriteAsJsonAsync(new { message = "«адача не найдена" });
+        // получаем пользовател€ по id
+        MyTask? MyTask = db.MyTasks.FirstOrDefault((t) => t.Id == id);
+        // если пользователь найден, удал€ем его
+        if (MyTask != null)
+        {
+            db.MyTasks.Remove(MyTask);
+            db.SaveChanges();
+            await response.WriteAsJsonAsync(MyTask);
+        }
+
+        // если не найден, отправл€ем статусный код и сообщение об ошибке
+        else
+        {
+            response.StatusCode = 404;
+            await response.WriteAsJsonAsync(new { message = "«адача не найдена" });
+        }
     }
 }
- 
+
 async Task CreateMyTask(HttpResponse response, HttpRequest request)
 {
     try
@@ -102,7 +105,11 @@ async Task CreateMyTask(HttpResponse response, HttpRequest request)
             // устанавливаем id дл€ нового пользовател€
             myTask.Id = Guid.NewGuid().ToString();
             // добавл€ем пользовател€ в список
-            myTasks.Add(myTask);
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.MyTasks.Add(myTask);
+                db.SaveChanges();
+            }
             await response.WriteAsJsonAsync(myTask);
         }
         else
@@ -116,7 +123,7 @@ async Task CreateMyTask(HttpResponse response, HttpRequest request)
         await response.WriteAsJsonAsync(new { message = "Ќекорректные данные" });
     }
 }
- 
+
 async Task UpdateMyTask(HttpResponse response, HttpRequest request)
 {
     try
@@ -125,18 +132,22 @@ async Task UpdateMyTask(HttpResponse response, HttpRequest request)
         MyTask? myTaskData = await request.ReadFromJsonAsync<MyTask>();
         if (myTaskData != null)
         {
-            // получаем пользовател€ по id
-            var myTask = myTasks.FirstOrDefault(t => t.Id == myTaskData.Id);
-            // если пользователь найден, измен€ем его данные и отправл€ем обратно клиенту
-            if (myTask != null)
+            using (ApplicationContext db = new ApplicationContext())
             {
-                myTask.Content = myTaskData.Content;
-                await response.WriteAsJsonAsync(myTask);
-            }
-            else
-            {
-                response.StatusCode = 404;
-                await response.WriteAsJsonAsync(new { message = "ѕользователь не найден" });
+                // получаем пользовател€ по id
+                var myTask = db.MyTasks.FirstOrDefault(t => t.Id == myTaskData.Id);
+                // если пользователь найден, измен€ем его данные и отправл€ем обратно клиенту
+                if (myTask != null)
+                {
+                    myTask.Content = myTaskData.Content;
+                    db.SaveChanges();
+                    await response.WriteAsJsonAsync(myTask);
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                    await response.WriteAsJsonAsync(new { message = "ѕользователь не найден" });
+                }
             }
         }
         else
@@ -149,9 +160,4 @@ async Task UpdateMyTask(HttpResponse response, HttpRequest request)
         response.StatusCode = 400;
         await response.WriteAsJsonAsync(new { message = "Ќекорректные данные" });
     }
-}
-public class MyTask
-{
-    public string Id { get; set; } = "";
-    public string Content { get; set; } = "";
 }
