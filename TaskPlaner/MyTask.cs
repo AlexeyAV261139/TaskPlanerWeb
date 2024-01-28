708b2b1f8a67ab88e165c724164ac8a4bacf38ce
Alexey <Alexey@DESKTOP-H2N279O>
2024-01-28 16:40:00 +0300

    Изменено. Отрефакторена кнопка отчистки значений

--- TaskPlaner/MyTask.cs
+++ TaskPlaner/MyTask.cs
@@ -1,6 +1,8 @@
 ﻿public class MyTask
 {
     public string Id { get; set; } = "";
+    public string Heading { get; set; } = "";
     public string Content { get; set; } = "";
+    public DateTime? Date { get; set; }  
 }
 
