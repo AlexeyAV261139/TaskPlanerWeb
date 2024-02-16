e57f862bb172e3136fc54114c389b9bbc8cfbfc5
Alexey <Alexey@DESKTOP-H2N279O>
2024-02-16 11:23:09 +0300

    Изменено. отрефакторена работа с базой данных, путем использования dependency injection

---
+++ TaskPlaner/Interfaces/IDbWorker.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskPlaner.Interfaces
+{
+    public interface IDbWorker
+    {
+        Task CreateMyTask(HttpResponse response, HttpRequest request);
+        Task DeleteMyTask(string? id, HttpResponse response);
+        Task GetAllMyTasks(HttpResponse response);
+        Task GetMyTask(string? id, HttpResponse response);
+        Task UpdateMyTask(HttpResponse response, HttpRequest request);
+    }
+}
