f48cad50809754818e6b96de2a4b858fde168046
Alexey <Alexey261139@mail.ru>
2024-02-16 20:09:11 +0300

    Добавлено. Набросок формы авторизации с помощью JWT токена

--- TaskPlaner/Program.cs
+++ TaskPlaner/Program.cs
@@ -1,17 +1,89 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+var people = new List<Person>
+ {
+    new Person("tom@gmail.com", "12345"),
+    new Person("bob@gmail.com", "55555")
+};
 
 var builder = WebApplication.CreateBuilder();
-string connection = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+string connection = builder.Configuration.GetConnectionString("DefaultConnection") ??
     throw new Exception("Не указана строка подключения");
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
-
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // указывает, будет ли валидироваться издатель при валидации токена
+            ValidateIssuer = true,
+            // строка, представляющая издателя
+            ValidIssuer = AuthOptions.ISSUER,
+            // будет ли валидироваться потребитель токена
+            ValidateAudience = true,
+            // установка потребителя токена
+            ValidAudience = AuthOptions.AUDIENCE,
+            // будет ли валидироваться время существования
+            ValidateLifetime = true,
+            // установка ключа безопасности
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            // валидация ключа безопасности
+            ValidateIssuerSigningKey = true,
+        };
+    });
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.MapGet("/login", async (context) =>
+{
 
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.SendFileAsync("wwwroot/html/Authorization.html");
+});
+
+app.MapPost("/login", (Person loginData) =>
+{
+    Person? person = people.FirstOrDefault(p => p.Email == loginData.Email &&
+    p.Password == loginData.Password);
+
+    if (person == null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
+
+    var jwt = new JwtSecurityToken(
+           issuer: AuthOptions.ISSUER,
+           audience: AuthOptions.AUDIENCE,
+           claims: claims,
+           expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+           signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = person.Email
+    };
+
+    return Results.Json(response);
+});
+
+
+app.Map("/data", [Authorize] () => new { message = "Hello World!" });
+
+#region
 app.MapGet("/api/tasks", async (ApplicationContext db) => await db.MyTasks.ToListAsync());
 
 app.MapGet("/api/tasks/{id:guid}", async (string id, ApplicationContext db) =>
@@ -53,6 +125,16 @@ app.MapDelete("/api/tasks/{id:guid}", async (string id, ApplicationContext db) =
     await db.SaveChangesAsync();
     return Results.Json(task);
 });
-
+#endregion 
 app.Run();
 
+public class AuthOptions
+{
+    public const string ISSUER = "MyAuthServer"; // издатель токена
+    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+    const string KEY = "mysupersecret_secretsecretsecretkey!123";   // ключ для шифрации
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
+
+record class Person(string Email, string Password);
